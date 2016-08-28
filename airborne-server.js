33698f7faa28660dff70299b3ae14ee4ccb7157b
Yuta Takahashi <takahashi.yuta.to2@is.naist.jp>
2016-08-28 20:06:23 +0900

    add /drone/control/drive api

--- airborne-server.js
+++ airborne-server.js
@@ -5,13 +5,13 @@ var RollingSpider = require('rolling-spider');
 
 var drone = new RollingSpider( { uuid: conf.droneID }) ;
 
-function check_drive_parameter( query ){
-  var option = { speed: 50, steps: 20 };
+function check_move_parameter( query ){
+  var param = { speed: 50, steps: 20 };
   if( 'speed' in query ){
     if( !isNaN(query.speed) ){
       var speed = parseInt(query.speed);
       if( speed >= 0 && speed <= 100 ){
-        option.speed = speed;
+        param.speed = speed;
       }
     }
   }
@@ -20,11 +20,53 @@ function check_drive_parameter( query ){
     if( !isNaN(query.steps) ){
       var steps = parseInt(query.steps);
       if( steps >= 0 && steps <= 100 ){
-        option.steps = steps;
+        param.steps = steps;
       }
     }
   }
-  return option;
+  return param;
+}
+
+function check_drive_parameter( query ){
+  var param = {tilt:0, forward:0, turn:0, up:0, steps: 20};
+  if( 'tilt' in query ){
+    if( !isNaN(query.tilt) ){
+      var tilt = parseInt(query.tilt);
+      param.tilt = tilt;
+    }
+  }
+
+  if( 'forward' in query ){
+    if( !isNaN(query.forward) ){
+      var forward = parseInt(query.forward);
+      param.forward = forward;
+    }
+  }
+
+  if( 'turn' in query ){
+    if( !isNaN(query.turn) ){
+      var turn = parseInt(query.turn);
+      param.turn = turn;
+    }
+  }
+
+  if( 'up' in query ){
+    if( !isNaN(query.up) ){
+      var up = parseInt(query.up);
+      param.up = up;
+    }
+  }
+
+  if( 'steps' in query ){
+    if( !isNaN(query.steps) ){
+      var steps = parseInt(query.steps);
+      if( steps >= 0 && steps <= 100 ){
+        param.steps = steps;
+      }
+    }
+  }
+
+  return param;
 }
 
 app.get('/', function(req, res) {
@@ -52,11 +94,11 @@ app.get('/drone/connect', function(req, res) {
 } );
 
 app.get('/drone/disconnect', function(req, res) {
-  if( drone.connected && !drone.takenOff ){
+  if( drone.connected && !drone.status.flying ){
     drone.disconnect( function() {
       res.send('drone#disconnected');
     } );
-  }else if( drone.takenOff ){
+  }else if( drone.status.flying ){
     res.send('can not disconnect');
   }
 } );
@@ -67,9 +109,8 @@ app.get('/drone/status', function(req, res) {
 } );
 
 app.get('/drone/control/takeoff', function(req, res) {
-  if( drone.connected && !drone.takenOff ){
+  if( drone.connected && !drone.status.flying ){
     drone.takeOff();
-    drone.takenOff = true
     setTimeout(function () {
       console.log('drone#takeOff');
       res.send('drone#takeoff');
@@ -78,9 +119,8 @@ app.get('/drone/control/takeoff', function(req, res) {
 } );
 
 app.get('/drone/control/land', function(req, res) {
-  if( drone.connected && drone.takenOff ){
+  if( drone.connected && drone.status.flying ){
     drone.land();
-    drone.takenOff = false
     setTimeout(function () {
       console.log('drone#land');
       res.send('drone#land');
@@ -89,104 +129,110 @@ app.get('/drone/control/land', function(req, res) {
 } );
 
 app.get('/drone/control/forward', function(req, res) {
-  var option = check_drive_parameter(req.query);
-  if( drone.connected && drone.takenOff ){
-    drone.forward( option );
+  var param = check_move_parameter(req.query);
+  if( drone.connected && drone.status.flying ){
+    drone.forward( param );
     setTimeout( function(){
       console.log('drone#forward');
       res.send('drone#forward');
-    }, option.steps * 12 );
+    }, param.steps * 12 );
   }else{
     res.send('drone can not control');
   }
 } );
 
 app.get('/drone/control/backward', function(req, res) {
-  var option = check_drive_parameter(req.query);
-  if( drone.connected && drone.takenOff ){
-    drone.backward( option );
+  var param = check_move_parameter(req.query);
+  if( drone.connected && drone.status.flying ){
+    drone.backward( param );
     setTimeout( function(){
       console.log('drone#backward');
       res.send('drone#backward');
-    }, option.steps * 12 );
+    }, param.steps * 12 );
   }else{
     res.send('drone can not control');
   }
 } );
 
 app.get('/drone/control/tiltright', function(req, res) {
-  var option = check_drive_parameter(req.query);
-  if( drone.connected && drone.takenOff ){
-    drone.tiltRight( option );
+  var param = check_move_parameter(req.query);
+  if( drone.connected && drone.status.flying ){
+    drone.tiltRight( param );
     setTimeout( function(){
       console.log('drone#tiltright');
       res.send('drone#tiltright');
-    }, option.steps * 12 );
+    }, param.steps * 12 );
   }else{
     res.send('drone can not control');
   }
 } );
 
 app.get('/drone/control/tiltleft', function(req, res) {
-  var option = check_drive_parameter(req.query);
-  if( drone.connected && drone.takenOff ){
-    drone.tiltLeft( option );
+  var param = check_move_parameter(req.query);
+  if( drone.connected && drone.status.flying ){
+    drone.tiltLeft( param );
     setTimeout( function(){
       console.log('drone#tiltleft');
       res.send('drone#tiltleft');
-    }, option.steps * 12 );
+    }, param.steps * 12 );
   }else{
     res.send('drone can not control');
   }
 } );
 
 app.get('/drone/control/turnright', function(req, res) {
-  var option = check_drive_parameter(req.query);
-  if( drone.connected && drone.takenOff ){
-    drone.turnRight( option );
+  var param = check_move_parameter(req.query);
+  if( drone.connected && drone.status.flying ){
+    drone.turnRight( param );
     setTimeout( function(){
       console.log('drone#turnright');
       res.send('drone#turnright');
-    }, option.steps * 12 );
+    }, param.steps * 12 );
   }else{
     res.send('drone can not control');
   }
 } );
 
 app.get('/drone/control/turnleft', function(req, res) {
-  var option = check_drive_parameter(req.query);
-  if( drone.connected && drone.takenOff ){
-    drone.turnLeft( option );
+  var param = check_move_parameter(req.query);
+  if( drone.connected && drone.status.flying ){
+    drone.turnLeft( param );
     setTimeout( function(){
       console.log('drone#turnleft');
       res.send('drone#turnleft');
-    }, option.steps * 12 );
+    }, param.steps * 12 );
   }else{
     res.send('drone can not control');
   }
 } );
 
 app.get('/drone/control/up', function(req, res) {
-  var option = check_drive_parameter(req.query);
-  if( drone.connected && drone.takenOff ){
-    drone.up( option );
+  var param = check_move_parameter(req.query);
+  if( drone.connected && drone.status.flying ){
+    drone.up( param );
     setTimeout( function(){
       console.log('drone#up');
       res.send('drone#up');
-    }, option.steps * 12 );
+    }, param.steps * 12 );
   }else{
     res.send('drone can not control');
   }
 } );
 
-app.get('/drone/control/down', function(req, res) {
-  var option = check_drive_parameter(req.query);
-  if( drone.connected && drone.takenOff ){
-    drone.down( option );
+app.get('/drone/control/drive', function(req, res) {
+  var param = check_drive_parameter(req.query);
+  if( drone.connected && drone.status.flying ){
+    var drive_param = {tilt:0, forward:0, turn:0, up:0};
+    drive_param.tilt = param.tilt;
+    drive_param.forward = param.forward;
+    drive_param.turn = param.turn;
+    drive_param.up = param.up;
+
+    drone.drive( drive_param, param.steps );
     setTimeout( function(){
-      console.log('drone#down');
-      res.send('drone#down');
-    }, option.steps * 12 );
+      console.log('drone#drive');
+      res.send('drone#drive');
+    }, param.steps * 12 );
   }else{
     res.send('drone can not control');
   }
